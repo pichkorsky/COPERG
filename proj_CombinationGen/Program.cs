e54d3568f2aa05bc403ba2b0ac26d07ac01e173e
AlStorm <Pichkorsky@gmail.com>
2017-07-26 20:23:28 +0300

    Add project files.

---
+++ proj_CombinationGen/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Permutations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                dowork();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+           
+        }
+
+        static void dowork()
+        {            
+
+
+            string[] tstInput =
+                {
+                "A",
+                "B",
+                "C",
+                "D"
+            };
+
+            Permutations.generate_Permutations(tstInput);
+
+
+            Console.WriteLine(Permutations.Counter);
+        }        
+    }
+}
